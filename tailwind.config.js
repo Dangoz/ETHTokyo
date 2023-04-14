772006ade606721ad369f493d1de7b802eb92cbe
Dango <jason543211@hotmail.com>
2023-04-15 00:30:36 +0900

    feat: setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        background: '#0d1117',
+        foreground: '#161b22',
+        bgBlue: '#111827',
+        bgGrey: '#374151',
+        primary: '#2563eb',
+        secondary: 'rgba(192, 132, 252, 0.5)',
+      },
+      boxShadow: {
+        custom: "0 0 10px #29d, 0 0 5px #29d",
+      },
+      gradientColorStops: {
+        gradientOne: '#FFB7C5',
+        gradientTwo: '#0A3D62',
+        gradientThree: '#FFC300',
       },
     },
   },
+  
   plugins: [],
 }
