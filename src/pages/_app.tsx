16b2d1f6e9ea15b192b33cb7290604c40de1244f
Dango <jason543211@hotmail.com>
2023-04-15 10:12:32 +0900

    fix: ranspilePackages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { polygonMumbai, polygon } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { LensConfig, development, LensProvider } from '@lens-protocol/react-web'
+import { bindings as wagmiBindings } from '@lens-protocol/wagmi'
+import { handleError } from '@/common/notification'
+
+const { provider, webSocketProvider } = configureChains([polygon, polygonMumbai], [publicProvider()])
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+
+const lensConfig: LensConfig = {
+  bindings: wagmiBindings(),
+  environment: development,
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <LensProvider config={lensConfig} onError={handleError}>
+        <Component {...pageProps} />
+      </LensProvider>
+    </WagmiConfig>
+  )
 }
